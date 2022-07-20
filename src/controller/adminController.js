5ebb4cc18bfe648f5446683813fa6fa3455ef3c3
Hoàng Nam <nnam44565@gmail.com>
2022-07-20 12:06:29 +0700

    edit delete

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -2,6 +2,7 @@ import md5 from 'md5';
 import pool from '../config/connectDatabase'
 //[GET]
 class AdminController {
+    //[GET] Page login
     login = (req, res) => {
         if (req.signedCookies.email) {
             res.redirect('/admin/home');
@@ -9,12 +10,14 @@ class AdminController {
         }
         return res.render('admin/login.ejs', { err: '' })
     }
+
+    //GET home page login
     getHomepage = (req, res) => {
         if (req.signedCookies.email) {
             return res.render('admin/home.ejs', { data: req.data[0], url: req.url })
         }
     }
-    //[POST]
+    //GET checklogin page
     checkuser = async (req, res) => {
         const { user_name, password } = req.body;
         const hashPassword = md5(Number(password));
@@ -25,7 +28,7 @@ class AdminController {
             res.cookie('email', rows[0].email, {
                 signed: true,
             });
-            res.render('admin/home.ejs', { data: rows[0] })
+            res.render('admin/home.ejs', { data: rows[0], url: req.url })
             return;
         }
         else {
@@ -33,17 +36,121 @@ class AdminController {
             return;
         }
     }
+    //GET User by id
+    editUser = async (req, res) => {
+        const { id } = req.params;
+        const sql = `SELECT * FROM thanhvien WHERE id_thanhvien = ${id}`;
+        const [rows] = await pool.execute(sql);
+        res.render('admin/editUser.ejs', { data: rows[0], url: req.url })
+        // return;
+    }
 
+    //GET User update
+    updateUser = async (req, res) => {
+        const { id } = req.params;
+        const { sdt, cmnd, address, name, email } = req.body;
+        const sql = `UPDATE thanhvien SET sdt = '${sdt}', cmnd = '${cmnd}', address = '${address}', name = '${name}', email = '${email}' WHERE id_thanhvien = ${id}`;
+        await pool.execute(sql);
+        res.redirect('/admin/home');
+    }
+    // LOGOUT
     logout = (req, res) => {
         res.clearCookie('email');
         return res.redirect('/admin');
     }
 
-
+    //GET account page
     account = async (req, res) => {
         const sql = `SELECT * FROM thanhvien`;
         const [rows] = await pool.execute(sql);
         return res.render('admin/account.ejs', { data: rows, url: req.url })
+    }
+
+    // delete account page
+    deleteAccount = async (req, res) => {
+        const { id } = req.params;
+        const sql = `DELETE FROM thanhvien WHERE id_thanhvien = ${id}`;
+        await pool.execute(sql);
+        return res.redirect('/admin/account');
+    }
+
+    //GET categories page
+    categories = async (req, res) => {
+        const sql = `SELECT * FROM dmsanpham`;
+        const [rows] = await pool.execute(sql);
+        return res.render('admin/categories.ejs', { data: rows, url: req.url })
+    }
+
+    // delete categories page
+    deleteCategory = async (req, res) => {
+        const { id } = req.params;
+        const sql = `DELETE FROM dmsanpham WHERE id_dm = ${id}`;
+        await pool.execute(sql);
+        return res.redirect('/admin/categories');
+    }
+
+    // edit categories page
+    editCategory = async (req, res) => {
+        //get category by id
+        const { id } = req.params;
+        const sql = `SELECT * FROM dmsanpham WHERE id_dm = ${id}`;
+        const [rows] = await pool.execute(sql);
+        return res.render('admin/editCategory.ejs', { data: rows[0], url: req.url })
+    }
+
+    // update categories 
+    updateCategory = async (req, res) => {
+        const { ten_dm } = req.body;
+        const { id } = req.params;
+        const sql = `UPDATE dmsanpham SET ten_dm = '${ten_dm}' WHERE id_dm = ${id}`;
+        await pool.execute(sql);
+        console.log("Checkk SQL>>>>>>>", sql);
+        return res.redirect('/admin/categories');
+    }
+
+
+    // Get bills page
+    bills = async (req, res) => {
+        const sql = `SELECT * FROM hoa_don`;
+        const [rows] = await pool.execute(sql);
+        return res.render('admin/bills.ejs', { data: rows, url: req.url })
+    }
+
+    // delete bills 
+    deleteBill = async (req, res) => {
+        const { id } = req.params;
+        const sql = `DELETE FROM hoa_don WHERE id  = ${id}`;
+        console.log("Checkk SQL:>>>>>", sql);
+        await pool.execute(sql);
+        return res.redirect('/admin/bills');
+    }
+    // GET products page
+    products = async (req, res) => {
+        const sql = `SELECT * FROM sanpham`;
+        const [rows] = await pool.execute(sql);
+        return res.render('admin/products.ejs', { data: rows, url: req.url })
+    }
+    // delete products
+    deleteProduct = async (req, res) => {
+        const { id } = req.params;
+        const sql = `DELETE FROM sanpham WHERE id_sp = ${id}`;
+        await pool.execute(sql);
+        return res.redirect('/admin/products');
+    }
+    // GET edit product page
+    editProduct = async (req, res) => {
+        const { id } = req.params;
+        const sql = `SELECT * FROM sanpham WHERE id_sp = ${id}`;
+        const [rows] = await pool.execute(sql);
+        return res.render('admin/editProduct.ejs', { data: rows[0], url: req.url })
+    }
+    // update product
+    updateProduct = async (req, res) => {
+        const { ten_sp, gia_sp, trang_thai, ma_dm } = req.body;
+        const { id } = req.params;
+        const sql = `UPDATE sanpham SET ten_sp = '${ten_sp}', gia_sp = ${gia_sp}, trang_thai = '${trang_thai}', id_dm = ${ma_dm} WHERE id_sp = ${id}`;
+        await pool.execute(sql);
+        return res.redirect('/admin/products');
     }
 }
 
--- src/router/admin.js
+++ src/router/admin.js
@@ -4,8 +4,22 @@ import authMiddleware from '../middlewares/auth.middlewares'
 const router = express.Router();
 router.get('/home', authMiddleware.requireAuth, adminController.getHomepage);
 router.post('/home', adminController.checkuser);
+router.post('/user/edit/:id', adminController.editUser);
+router.post('/user/update/:id', adminController.updateUser);
 router.get('/logout', adminController.logout);
 router.get('/account', authMiddleware.requireAuth, adminController.account);
+router.post('/delete/:id', authMiddleware.requireAuth, adminController.deleteAccount);
+router.get('/categories', authMiddleware.requireAuth, adminController.categories);
+router.post('/categories/delete/:id', authMiddleware.requireAuth, adminController.deleteCategory);
+router.post('/categories/edit/:id', authMiddleware.requireAuth, adminController.editCategory);
+router.post('/categories/update/:id', authMiddleware.requireAuth, adminController.updateCategory);
+router.get('/bills', authMiddleware.requireAuth, adminController.bills);
+router.post('/bills/delete/:id', authMiddleware.requireAuth, adminController.deleteBill);
+router.get('/products', authMiddleware.requireAuth, adminController.products);
+router.post('/products/delete/:id', authMiddleware.requireAuth, adminController.deleteProduct);
+router.post('/products/edit/:id', authMiddleware.requireAuth, adminController.editProduct);
+router.post('/products/update/:id', authMiddleware.requireAuth, adminController.updateProduct);
+
 router.get('/', adminController.login);
 module.exports = router;
 
